3903c1520110f4533647134a32f8041b2be38a27
Nikhilnama18 <nikhilnama1111@gmail.com>
2021-07-26 19:11:14 +0530

    Added Add Org in DashBoard

--- front_end/ui-vue/src/views/Dashboard.vue
+++ front_end/ui-vue/src/views/Dashboard.vue
@@ -2,7 +2,13 @@
   <div class="containers">
     <h1>Organisations</h1>
     <Button @click="addorg" title="Add Organisation" color="green" />
-    <Organisations v-if="logged" :Orgs="Orgs" />
+    <AddOrg @refresh="pushOrg" @cancel="cancel" v-if="addOrg" />
+    <div>
+      <h3>Organisation Name Location GST Number</h3>
+    </div>
+    <div>
+      <Organisations v-if="logged" :Orgs="Orgs" />
+    </div>
     <!-- <Button @click="" title="Cancel" color="red" /> -->
     <p v-if="true"></p>
   </div>
@@ -11,44 +17,53 @@
 <script>
 import Button from "../components/Button.vue";
 import Organisations from "../components/Organisations.vue";
+import AddOrg from "../components/AddOrg.vue";
 export default {
   name: "Dashboard",
   data() {
     return {
-      u_id: Number,
-      jwtToken: String,
       Orgs: Array,
       logged: false,
+      addOrg: false,
     };
   },
   components: {
     Button,
     Organisations,
+    AddOrg,
   },
   methods: {
     addorg() {
-      console.log("Entred");
+      this.addOrg = !this.addOrg;
+    },
+    cancel() {
+      this.addOrg = false;
     },
     async fetchorg() {
-      this.u_id = localStorage.getItem("u_id");
-      this.jwtToken = localStorage.getItem("jwtToken");
-      const response = await fetch(`api/users/${this.u_id}/orgs`, {
-        method: "GET",
-        headers: {
-          "Content-type": "application/json",
-          authorization: `Bearer ${this.jwtToken}`,
-        },
-      });
+      this.addOrg = false;
+      const response = await fetch(
+        `api/users/${localStorage.getItem("u_id")}/orgs`,
+        {
+          method: "GET",
+          headers: {
+            "Content-type": "application/json",
+            authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
+          },
+        }
+      );
       if (response.status === 200) {
         let result = await response.json();
         this.logged = true;
         this.Orgs = result.data;
+        console.log(result.data);
       }
+    },
+    pushOrg(data) {
+      this.Orgs.push(data[0]);
     },
   },
   created() {
     this.fetchorg();
-    console.log("Dashboard comp created.");
   },
 };
 </script>
