42cee17725bd4f34da201721088e2d6d3dab4167
Suhasnama <suhasnama23@gmail.com>
2021-08-13 14:45:22 +0530

    Added make payment in the bills component .

--- front_end/src/components/Bill.vue
+++ front_end/src/components/Bill.vue
@@ -20,7 +20,11 @@
             : { background: 'Pink' },
           'font-size: 120%',
         ]"
-        @click="isUpdateClicked = !isUpdateClicked"
+        @click="
+          isUpdateClicked = !isUpdateClicked;
+          isMakePaymentClicked = false;
+          updateAmmount = bill.ammount;
+        "
         alt="Edit Bill"
       >
         <i
@@ -45,6 +49,7 @@
         <i class="fa fa-trash" aria-hidden="true"></i>
       </button>
     </div>
+
     <h3 v-show="bill.due_ammount == 0">Bill Closed</h3>
     <p></p>
     <label> Bill ID : </label>
@@ -61,14 +66,16 @@
         v-model="updateAmmount"
       />&#x20B9;
       <p></p>
-      <label> Due Ammount : </label>
+
+      <!-- Disabling the abilit to directly update due amount  -->
+      <!-- <label> Due Ammount : </label>
       <input
         class="linkField"
         type="number"
         name="update_due_ammount"
         id="update_due_ammount"
         v-model="updateDueAmmount"
-      />&#x20B9;
+      />&#x20B9; -->
     </div>
 
     <div v-show="!isUpdateClicked">
@@ -98,6 +105,81 @@
     <div v-show="isUpdateClicked">
       <Button title="Update" color="purple" @click="updateBill" />
     </div>
+
+    <!-- Make Payment -->
+    <div v-show="isMakePaymentClicked">
+      <label> Paying Ammount : </label>
+      <input
+        class="linkField"
+        type="number"
+        name="payment_ammount"
+        id="payment_ammount"
+        v-model="paymentAmmount"
+      />&#x20B9;
+    </div>
+
+    <p></p>
+    <div v-show="this.bill.due_ammount != 0">
+      <p>
+        <b v-show="paymentIsGreater"
+          >Payment amount is greater than due amount</b
+        >
+      </p>
+      <Button
+        v-show="isMakePaymentClicked"
+        @click="
+          isMakePaymentClicked = !isMakePaymentClicked;
+          paymentIsGreater = false;
+          paymentAmmount = 0;
+        "
+        title="Cancel Payment"
+      />
+
+      <Button
+        color="Indigo"
+        class="edit-button"
+        @click="makePaymentClicked"
+        alt="Make Payment"
+        title="Make Payment"
+      />
+
+      <!-- <button
+        v-show="isMakePaymentClicked"
+        class="edit-button"
+        :style="[
+          bill.due_ammount == 0
+            ? { background: 'Pink' }
+            : bill.due_ammount < bill.ammount / 2
+            ? { background: 'LightGreen' }
+            : { background: 'Lavender' },
+          'font-size: 120%',
+        ]"
+        @click="
+          isMakePaymentClicked = !isMakePaymentClicked;
+          paymentIsGreater = false;
+          paymentAmmount = 0;
+        "
+        alt="Cancel Payment"
+      >
+        Cancel Payment
+      </button>
+
+      <button
+        class="edit-button"
+        :style="[
+          bill.due_ammount == 0
+            ? { background: 'Pink' }
+            : bill.due_ammount < bill.ammount / 2
+            ? { background: 'LightGreen' }
+            : { background: 'Lavender' },
+          'font-size: 120%',
+        ]"
+        @click="makePaymentClicked"
+        alt="Make Payment"
+      >
+        Make Payment
+      </button> -->
+    </div>
   </div>
 </template>
 
@@ -112,8 +194,11 @@ export default {
   data() {
     return {
       isUpdateClicked: false,
+      isMakePaymentClicked: false,
       updateAmmount: this.bill.ammount,
-      updateDueAmmount: this.bill.due_ammount,
+    //   updateDueAmmount: this.bill.due_ammount,
+      paymentAmmount: 0,
+      paymentIsGreater: false,
     };
   },
   emits: ["delete-bill", "update-bill"],
@@ -141,7 +226,7 @@ export default {
             },
             body: JSON.stringify({
               ammount: parseInt(this.updateAmmount),
-              due_ammount: parseInt(this.updateDueAmmount),
+            //   due_ammount: parseInt(this.updateDueAmmount),
             }),
           }
         );
@@ -151,14 +236,15 @@ export default {
           if (result.statusCode == "10000") {
             this.isUpdateClicked = false;
 
+            // No dependencies on this event emit currently.
             this.$emit("update-bill", {
               ammount: parseInt(this.updateAmmount),
-              due_ammount: parseInt(this.updateDueAmmount),
+            //   due_ammount: parseInt(this.updateDueAmmount),
               b_id: this.bill.b_id,
             });
             // Update the bill in UI.
             this.bill.ammount = parseInt(this.updateAmmount);
-            this.bill.due_ammount = parseInt(this.updateDueAmmount);
+            // this.bill.due_ammount = parseInt(this.updateDueAmmount);
           } else {
             alert("Unable to update bill.");
           }
@@ -190,6 +276,67 @@ export default {
         this.$router.push("Dashboard");
       }
       return o_id;
+    },
+
+    async makePaymentClicked() {
+      // Close the update fields
+      this.isUpdateClicked = false;
+
+      if (!this.isMakePaymentClicked || this.paymentAmmount == 0) {
+        this.isMakePaymentClicked = true;
+        return;
+      }
+      this.paymentIsGreater = false;
+      if (
+        confirm(
+          `Are you sure , you want to make payment towards the bill with id : ${this.bill.b_id}`
+        )
+      ) {
+        // To show cancel button
+        this.isMakePaymentClicked = true;
+
+        // Validate this.paymentAmmount value
+        // Payment is more than due.
+        if (this.paymentAmmount > this.bill.due_ammount) {
+          //   alert("Your payment is greater than due.");
+          this.paymentIsGreater = true;
+          return;
+        }
+
+        const newDueAmmount = this.bill.due_ammount - this.paymentAmmount;
+        const response = await fetch(
+          `api/users/${this.getUserId()}/orgs/${this.getOrgId()}/bills/${
+            this.bill.b_id
+          }`,
+          {
+            method: "PUT",
+            headers: {
+              "Content-type": "application/json",
+              authorization: `Bearer ${this.getJwtToken()}`,
+            },
+            body: JSON.stringify({
+              due_ammount: parseInt(newDueAmmount),
+            }),
+          }
+        );
+
+        if (response.status == 200) {
+          let result = await response.json();
+          if (result.statusCode == "10000") {
+            this.isMakePaymentClicked = false;
+
+            // Update the bill in UI.
+            this.bill.due_ammount = parseInt(newDueAmmount);
+          } else {
+            alert("Unable to update payment of bill. Please try again");
+          }
+        } else {
+          alert("Unable to update payment of bill. Please try again");
+        }
+
+        this.paymentAmmount = 0;
+        this.isMakePaymentClicked = false;
+      }
     },
   },
 };
